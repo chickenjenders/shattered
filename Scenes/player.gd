f717ccf826364b75a4e813f21c0375a9f80197f3
chickenjenders <tylerjenny8@gmail.com>
2024-11-12 00:28:50 -0500

    yes

--- Scenes/player.gd
+++ Scenes/player.gd
@@ -1,19 +1,29 @@
 extends AnimatedSprite2D
 
-var keys_to_check = [KEY_SPACE, KEY_A, KEY_S, KEY_D]  # Add the keys you want to check
-var default_frame = 0  # Set this to your default frame index
+var keys_to_check = [KEY_S, KEY_C, KEY_D, KEY_F, KEY_SPACE, KEY_Q] # Add the keys you want to check
+var default_frame = 0 # Set this to your default frame index
+
+var can_interact_with_mirror = false
 
 func _ready():
 	frame = default_frame
 
-func _process(delta):
+func _process(_delta):
 	var keys_pressed = 0
-	
-	for key in keys_to_check:
-		if Input.is_key_pressed(key):
-			keys_pressed += 1
-	
-	if keys_pressed > 0:
+
+	print(frame)
+	if can_interact_with_mirror:
+		for i in range(0, keys_to_check.size()):
+			if Input.is_key_pressed(keys_to_check[i]):
+				keys_pressed = i + 1
+
 		frame = default_frame + keys_pressed
-	else:
-		frame = default_frame
+
+func _on_main_toggle_mirror_interaction() -> void:
+	can_interact_with_mirror = not can_interact_with_mirror
+
+
+func _on_main_end_dialogue() -> void:
+	frame = 6
+	can_interact_with_mirror = false
+	print("yeehaw")
--- Scripts/dialogue_manager.gd
+++ Scripts/dialogue_manager.gd
@@ -1,48 +1,60 @@
 extends Node
 
-@onready var text_box_scene = preload("res://Scenes/text_box.tscn")
+@export var text_box: MarginContainer
 
-var dialog_lines: Array[String] = []
+var dialog_lines: Array[String] = [
+		"Me: OH MY GOD! I look worse than I thought. I'm horrifying, what should I do?",
+		"Store Clerk: Don't worry, we can fix you right up! Let's start with your lips, they look a bit dry. Press and hold 'S' to apply some lip moisturizer.",
+		"Me: Wow, I guess it's better? No, it might be worse.",
+		"Store Clerk: No, no! It's definitely better! Press and hold 'C' to try this skin cream to brighten your complexion.",
+		"Me: Something looks wrong here.",
+		"Store Clerk: It's such an improvement! Maybe a bit more for the skin? Press and hold 'D' to apply powder for evening out your skint tone.",
+		"Me: I'm pretty sure I've seen an animatronic that looks like this in the Halloween store.",
+		"Store Clerk: No, way! You're so stunning! We need to bring more attention to your eyes. Press and hold 'F' to try this eye cream to make them pop!",
+		"Me: Well, you werent lying. Do I really look better?",
+		"Store Clerk: Of course! These are the best and most popular products out there. You just can't live without them! That goes for our clothes too, here press 'SPACE' to switch to this new top we just got in!",
+		"Me: All of these things are pretty expensive.",
+		"Store Clerk: Well, you know what they say about quality of products. Speaking of, one last thing! You NEED this matching bag, your outfit just isn't complete without it! Press and hold 'Q' to try on.",
+		"Me: Do I really need all this to be considered beautiful? It's just too much, and it will never be enough anyway. I think its all just about profiting off my insecurities. I don't even think I look any different. Sigh, let's go home and eat a sweet treat.",
+	]
 var current_line_index = 0
 
-var text_box
+signal toggle_mirror_interaction
+
+signal end_dialogue
+
 var text_box_position: Vector2
 
 var is_dialog_active = false
-var can_advance_line = false
 
-func start_dialog(position: Vector2, lines: Array[String]):
+func _ready():
+	# Wait until the text_box is assigned
+	while text_box == null:
+		await get_tree().process_frame
+	start_dialog()
+
+func start_dialog():
 	if is_dialog_active:
 		return
 
-	dialog_lines = lines
-	text_box_position = position
 	_show_text_box()
 
 	is_dialog_active = true
-func _show_text_box():
-	text_box = text_box_scene.instantiate()
-	text_box.finished_displaying.connect(_on_text_box_finished_displaying)
-	get_tree().root.add_child(text_box)
-	text_box.global_position = text_box_position
-	text_box.display_text(dialog_lines[current_line_index])
-	can_advance_line = false
 
-func _on_text_box_finished_displaying():
-	can_advance_line = true
+func _show_text_box():
+	text_box.display_text(dialog_lines[current_line_index])
 
 func _unhandled_input(event):
 	if (
 		event.is_action_pressed("advance_dialogue") &&
-		is_dialog_active &&
-		can_advance_line
+		is_dialog_active
 	):
-		text_box.queue_free()
 		
 		current_line_index += 1
-		if current_line_index >= dialog_lines.size():
+		if current_line_index == 1:
+			toggle_mirror_interaction.emit()
+		if current_line_index == dialog_lines.size() - 1:
+			end_dialogue.emit()
 			is_dialog_active = false
-			current_line_index = 0
-			return
-			
+	
 		_show_text_box()
